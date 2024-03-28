4430c2bf5d7f213c3819afb5fc1730e3a039f026
Vosik <vosikvos@gmail.com>
2024-03-28 12:54:09 +0200

    Initial Commit

---
+++ Cache/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cache
+//
+//  Created by Саша Восколович on 28.03.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
