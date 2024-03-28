fb8d5d51ea2deca599721cd71a0edb6426ec7644
Vosik <vosikvos@gmail.com>
2024-03-28 23:44:03 +0200

    F - added save function

--- Cache/Cache.swift
+++ Cache/Cache.swift
@@ -8,13 +8,55 @@
 import Foundation
 
 
-protocol Cache {
+public protocol Cache {
     
-    associatedtype Element
-    
-    func get(_ element: Element) -> Element
-    func update(from old: Element, to new: Element)
+    associatedtype Element: Hashable
+
+    func get(_ element: Element) -> Element?
+    func update(_ element: Element)
     func add(_ element: Element)
 }
 
+final class Storage<E: Cache> {
+    
+    typealias Element = E.Element
+  
+    
+    private let cache: E
+   
+    
+    init(cache: E) {
+        self.cache = cache
+       
+    }
+    
+    func save(_ element: Element) {
+        cache.add(element)
+    }
+    
+//    private func isExist(_ element: Element) -> Bool {
+//        if let e = cache.get(element) {
+//            return e.id == element
+//        }
+//        return false
+//    }
+}
+
+
+//extension UserDefaults {
+//    
+//    func themes(forKey key: String) -> [Theme] {
+//        if let jsonData = data(forKey: key),
+//           let decodedThemes = try? JSONDecoder().decode([Theme].self, from: jsonData) {
+//            return decodedThemes
+//        } else {
+//            return []
+//        }
+//    }
+//    
+//    func set(_ themes: [Theme], forKey key: String) {
+//        let data = try? JSONEncoder().encode(themes)
+//        set(data, forKey: key)
+//    }
+//}
 
--- CacheTests/CacheTests.swift
+++ CacheTests/CacheTests.swift
@@ -8,49 +8,67 @@
 import XCTest
 @testable import Cache
 
+
+
 final class CacheTests: XCTestCase {
 
-   
-    func test_add_twoElements_shouldIncreaseCount() {
-        let sut = CacheMock()
-        let test: [String: String] = ["1": "Test"]
-        let test2: [String: String] = ["2": "Test2"]
+    
+    func test_add_twoElements_shouldSaveElements() {
        
+        let sut = makeSUT()
+        let test: String = "Test"
+        let test2: String = "Test2"
         
-        XCTAssertEqual(sut.callCount, 0)
+        XCTAssertEqual(cache.elements.count, 0)
        
-        sut.add(test)
+        sut.save(test)
+        XCTAssertEqual(cache.elements[test], test)
+        XCTAssertEqual(cache.elements.count, 1)
         
-        XCTAssertEqual(sut.lastAddedElement, test)
-        XCTAssertEqual(sut.callCount, 1)
         
-        sut.add(test2)
-        
-        XCTAssertEqual(sut.lastAddedElement, test2)
-        XCTAssertEqual(sut.callCount, 2)
-        
+        sut.save(test2)
+        XCTAssertEqual(cache.elements[test2], test2)
+        XCTAssertEqual(cache.elements.count, 2)
     }
     
+//    func test_get_withNotEmptyCache_shouldReturnElement() {
+//        let sut = CacheMock()
+//        let test: String = "Test"
+//        
+//        XCTAssertEqual(sut.elements.count, 0)
+//        
+//        sut.add(test)
+//        
+//        XCTAssertEqual(sut.get(test), test)
+//    }
+//    
+//    
+    
+    
+    // MARK: - Helpers
     
     private class CacheMock: Cache {
-       
-        private(set) var callCount: Int = 0
-        private(set) var lastAddedElement: [String: String] = [:]
+        
+        private(set) var elements: [String: String] = [:]
         var callback: ([String: String]) -> Void = { _ in }
         
-        func add(_ element: [String: String]) {
-            lastAddedElement = element
-            callCount += 1
+        func get(_ element: String) -> String? {
+            return elements[element]
         }
         
-        func get(_ element: [String: String]) -> [String: String] {
-            
-            return [:]
+        func add(_ element: String) {
+            elements[element] = element
         }
-        
-        
-        func update(from old: [String: String], to new: [String: String]) {
+
+        func update(_ element: String) {
             
         }
+    }
+    
+    private let cache: CacheMock = CacheMock()
+    
+    private func makeSUT() -> Storage<CacheMock> {
+        let sut = Storage(cache: cache)
+        return sut
     }
 }
